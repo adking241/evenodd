e381d1e25a4559876bc9f13933e26994732ba049
ANITA KING <ADKING>
2017-07-12 14:54:45 -0400

    wombat failure

--- evenodd_test.rb
+++ evenodd_test.rb
@@ -18,5 +18,9 @@ class TestEvenOdd < Minitest::Test #camelcase with class name otherwise copy all
 	def test_input3_returns_odd
 		assert_equal("odd", evenodd_function(3))
 	end
+
+	def test_input_wombat_returns_no_wombats_allowed
+		assert_equal("no wombats allowed", evenodd_function"wombat")
+	end
 	
 end #have to end the class like we end each function
